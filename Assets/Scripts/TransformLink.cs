199807e383578d2266ed74541b48f80b35004023
Tadhg O'Rourke <orourketadhg@gmail.com>
2020-12-01 11:50:24 +0000

    Initial Commit

---
+++ Assets/Scripts/TransformLink.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Transforms;
+using UnityEngine;
+
+public class TransformLink : MonoBehaviour {
+
+    public Entity TargetEntity;
+    public SyncType syncType;
+
+    private EntityManager _entityManager;
+    
+    void Start() {
+        _entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+
+    }
+    
+    void Update() {
+        SyncTarget();
+    }
+
+    private void SyncTarget() {
+        switch (syncType) {
+            case SyncType.EntityToThis:
+                transform.position = _entityManager.GetComponentData<LocalToWorld>(TargetEntity).Position;
+                transform.rotation = _entityManager.GetComponentData<LocalToWorld>(TargetEntity).Rotation;
+                break;
+                
+            case SyncType.ThisToEntityLocal:
+                _entityManager.SetComponentData(TargetEntity, new Translation() {
+                    Value = transform.localPosition
+                });
+                _entityManager.SetComponentData(TargetEntity, new Rotation() {
+                    Value = transform.localRotation
+                });
+                
+                break;
+        }
+    }
+    
+    public enum SyncType {
+        EntityToThis,
+        ThisToEntityLocal
+    }
+    
+}
