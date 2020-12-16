22664f7330ca7ab03a623a3895f836c5e224c205
Tadhg O'Rourke <orourketadhg@gmail.com>
2020-12-16 11:26:32 +0000

    Added Input monobehaviour to ECS system

---
+++ Assets/Scripts/ECS/Components/InputData.cs
@@ -0,0 +1,25 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace com.TUDublin.VRContaminationSimulation.ECS.Components {
+
+    [GenerateAuthoringComponent]
+    public struct InputData : IComponentData {
+
+        public ControllerHand Hand;
+        
+        public bool Grip;
+        public bool Trigger;
+        public bool PrimaryButton;
+        public bool SecondaryButton;
+        public float2 Joystick;
+        
+    }
+
+    public enum ControllerHand {
+        Left,
+        Right
+    }
+
+}
---
+++ Assets/Scripts/ECS/Systems/InputConversionSystem.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using com.TUDublin.VRContaminationSimulation.ECS.Components;
+using Unity.Entities;
+
+namespace com.TUDublin.VRContaminationSimulation.ECS.Systems {
+
+    public class InputConversionSystem : SystemBase {
+
+        private InputController _inputController;
+
+        protected override void OnStartRunning() {
+            _inputController = InputController.Instance;
+        }
+
+        protected override void OnUpdate() {
+            
+            Entities.WithoutBurst().ForEach((ref InputData input) => {
+                switch(input.Hand) {
+                    case ControllerHand.Left:
+                        input.Grip = _inputController.leftGrip;
+                        input.Trigger = _inputController.leftTrigger;
+                        input.PrimaryButton = _inputController.leftPrimaryBtn;
+                        input.SecondaryButton = _inputController.leftSecondaryBtn;
+                        input.Joystick = _inputController.leftJoystick;
+                        break;
+                    
+                    case ControllerHand.Right:
+                        input.Grip = _inputController.rightGrip;
+                        input.Trigger = _inputController.rightTrigger;
+                        input.PrimaryButton = _inputController.rightPrimaryBtn;
+                        input.SecondaryButton = _inputController.rightSecondaryBtn;
+                        input.Joystick = _inputController.rightJoystick;
+                        break;
+                    
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                    
+                }
+            }).Run();
+
+        }
+    }
+
+}
