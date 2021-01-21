0c709bf9b55b21ec6e7a237a09cce01d474f2ee3
Tadhg O'Rourke <orourketadhg@gmail.com>
2021-01-21 11:21:17 +0000

    Refactored RigToEntity + made hands static physics bodies

--- Assets/Scripts/Rig/RigToEntity.cs
+++ Assets/Scripts/Rig/RigToEntity.cs
@@ -1,4 +1,6 @@
-﻿using com.TUDublin.VRContaminationSimulation.ECS.Components;
+﻿using System;
+using System.Collections.Generic;
+using com.TUDublin.VRContaminationSimulation.ECS.Components;
 using Unity.Collections;
 using Unity.Entities;
 using Unity.Transforms;
@@ -8,35 +10,47 @@ namespace com.TUDublin.VRContaminationSimulation.Rig {
 
     public class RigToEntity : MonoBehaviour {
 
-        public RigType[] rig;
+        public Transform[] rig;
         private EntityManager _manager;
         private EntityQuery _rigQuery;
+        private readonly Dictionary<Transform, Entity> _rigEntityPairs = new Dictionary<Transform, Entity>();
 
         private void Start() {
             _manager = World.DefaultGameObjectInjectionWorld.EntityManager;
             _rigQuery = _manager.CreateEntityQuery(ComponentType.ReadOnly<XRRigData>());
             _rigQuery.SetChangedVersionFilter(typeof(XRRigData));
+            
+            var entities = _rigQuery.ToEntityArray(Allocator.Temp);
+            foreach (Entity entity in entities) {
+                XRRigData entityRigType = _manager.GetComponentData<XRRigData>(entity);
+                foreach (Transform node in rig) {
+                    RigType xrRigType = node.GetComponent<RigType>();
+                    
+                    if (xrRigType.type != entityRigType.Type) {
+                        continue;
+                    }
+                    
+                    _rigEntityPairs.Add(node.transform, entity);
+                }
+            }
+
         }
 
         private void LateUpdate() {
-            var entities = _rigQuery.ToEntityArray(Allocator.Temp);
-            foreach (Entity entity in entities) {
-                XRRigData rigType = _manager.GetComponentData<XRRigData>(entity);
+            UpdateRigPositions();
+        }
 
-                foreach (RigType node in rig) {
-                    if (node.type != rigType.Type) {
-                        continue;
-                    }
-
-                    _manager.SetComponentData(entity, new Translation() {
-                        Value = node.transform.position
-                    });
-                    _manager.SetComponentData(entity, new Rotation() {
-                        Value = node.transform.rotation
-                    });
-                }
+        private void UpdateRigPositions() {
+            foreach (var pair in _rigEntityPairs) {
+                _manager.SetComponentData(pair.Value, new Translation() {
+                    Value = pair.Key.position
+                });
+                _manager.SetComponentData(pair.Value, new Rotation() {
+                    Value = pair.Key.rotation
+                });
             }
         }
+        
     }
 
 }
