4c43c8551a46b1fa42b3adcbbf14b1d56059b752
Tadhg O'Rourke <orourketadhg@gmail.com>
2021-01-24 20:23:33 +0000

    Updated DOTS packages to latest version

---
+++ Assets/Scripts/ECS/Components/VirusParticleData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace com.TUDublin.VRContaminationSimulation.ECS.Components {
+
+    [GenerateAuthoringComponent]
+    public struct VirusParticleData : IComponentData {
+        public float2 virusParticleLifetimeRange;
+        [HideInInspector] public float remainingLifetime;
+
+    }
+
+}
