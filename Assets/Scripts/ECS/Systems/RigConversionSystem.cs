79cf604d9952b3dc9f5062236563c890a7ec4270
Tadhg O'Rourke <orourketadhg@gmail.com>
2021-01-26 14:32:37 +0000

    XR to DOTS conversion refactored to be done by DOTS system

---
+++ Assets/Scripts/ECS/Systems/RigConversionSystem.cs
@@ -0,0 +1,38 @@
+﻿using com.TUDublin.VRContaminationSimulation.ECS.Components;
+using com.TUDublin.VRContaminationSimulation.Rig;
+using Unity.Entities;
+using Unity.Transforms;
+using UnityEngine;
+
+namespace com.TUDublin.VRContaminationSimulation.ECS.Systems {
+
+    public class RigConversionSystem : SystemBase {
+
+        private RigType[] _rig;
+        
+        protected override void OnCreate() {
+            _rig = Object.FindObjectsOfType<RigType>();
+            
+        }
+
+        protected override void OnUpdate() {
+
+            Entities
+                .WithoutBurst()
+                .ForEach((ref Translation translation, ref Rotation rotation, in XRRigData rigData) => {
+                    foreach (RigType t in _rig) {
+                        if (rigData.Type != t.type) {
+                            continue;
+                        }
+
+                        translation.Value = t.transform.position;
+                        rotation.Value = t.transform.rotation;
+                        
+                    }
+                }).Run();
+
+        }
+        
+    }
+
+}
