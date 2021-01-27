561b60fe20be603c4e64d041cdfeb7c5cdddf6e2
Tadhg O'Rourke <orourketadhg@gmail.com>
2021-01-27 15:50:23 +0000

    blah

--- Assets/Scripts/Common/Interfaces/IVirusParticleSettings.cs
+++ Assets/Scripts/Common/Interfaces/IVirusParticleSettings.cs
@@ -3,13 +3,11 @@ using Unity.Mathematics;
 
 namespace com.TUDublin.VRContaminationSimulation.Common.Interfaces {
 
-    public interface IVirusParticleSpawnSettings {
+    public interface IVirusParticleSettings {
 
         Entity Prefab { get; set; }
         float2 Scale { get; set; }
         float2 InitialEmissionForce { get; set; }
-
-        
     }
 
 }
