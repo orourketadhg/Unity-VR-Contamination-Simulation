1dbf95216fe91b8e5ed1729d5e4d1020664c6769
Tadhg O'Rourke <orourketadhg@gmail.com>
2021-02-01 17:00:48 +0000

    Work on spawner

--- Assets/Scripts/DOTS/Systems/BreathingMechanicSpawnerSystem.cs
+++ Assets/Scripts/DOTS/Systems/BreathingMechanicSpawnerSystem.cs
@@ -1,6 +1,7 @@
 ﻿using com.TUDublin.VRContaminationSimulation.Common.Interfaces;
 using com.TUDublin.VRContaminationSimulation.Common.Structs;
 using com.TUDublin.VRContaminationSimulation.DOTS.Components.Authoring.Particles;
+using com.TUDublin.VRContaminationSimulation.DOTS.Components.Authoring.Spawner;
 using com.TUDublin.VRContaminationSimulation.DOTS.Components.Input;
 using Unity.Burst;
 using Unity.Collections;
@@ -20,28 +21,43 @@ namespace com.TUDublin.VRContaminationSimulation.DOTS.Systems {
         protected override void OnCreate() {
             _entityCommandBuffer = World.GetOrCreateSystem<EndFixedStepSimulationEntityCommandBufferSystem>();
 
-            _spawnerQuery = GetEntityQuery(ComponentType.ReadOnly<IBreathingMechanicSpawnerSettings>(),
-                ComponentType.ReadOnly<IVirusParticleSettings>());
+            _spawnerQuery = GetEntityQuery(typeof(MouthBreathSpawnerSettingsData));
 
         }
 
         protected override void OnUpdate() {
-
+            
             var ecb = _entityCommandBuffer.CreateCommandBuffer().AsParallelWriter();
-            var mouthBreathInput = GetSingleton<MouthBreathInputData>();
+            
+            // create a queue to store new particleSpawnData
+            var particleSpawnDataQueue = new NativeQueue<ParticleSpawnData>(Allocator.Temp);
+            
+            // create the job to generate the spawn data
+            var job = new GenerateVirusParticleSpawnData<MouthBreathSpawnerSettingsData>() {
+                particleSpawnData = particleSpawnDataQueue.AsParallelWriter()
+            };
 
-            var input = GetSingleton<MouthBreathInputData>();
+            // schedule and complete the spawn data generation
+            job.ScheduleParallel(_spawnerQuery, 1, Dependency).Complete();
 
+            // check if any data was generated
+            if (!particleSpawnDataQueue.IsEmpty()) {
+
+                // convert native queue to native array
+                var particleData = particleSpawnDataQueue.ToArray(Allocator.Temp);
+                var particleEntities = new NativeQueue<Entity>(Allocator.Temp);
+                
+            }
         }
         
         [BurstCompile]
-        private struct GenerateVirusParticleSpawnData<TSpawnerSettings> : IJobChunk {
+        private struct GenerateVirusParticleSpawnData<TSpawnerSettings> : IJobEntityBatch {
 
             // Store generated data for new particles
-            [WriteOnly] private NativeQueue<ParticleSpawnData>.ParallelWriter particleSpawnData;
-            
-            public void Execute(ArchetypeChunk chunk, int chunkIndex, int firstEntityIndex) {
+            [WriteOnly] public NativeQueue<ParticleSpawnData>.ParallelWriter particleSpawnData;
 
+
+            public void Execute(ArchetypeChunk batchInChunk, int batchIndex) {
                 var newParticle = new ParticleSpawnData() {
                     
                 };
