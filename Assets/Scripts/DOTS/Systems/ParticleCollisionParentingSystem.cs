cf56588f1381551edcdc2daea87173e14b02a714
Tadhg O'Rourke <orourketadhg@gmail.com>
2021-03-03 16:43:11 +0000

    particle sticking switching

--- Assets/Scripts/DOTS/Systems/ParticleCollisionParentingSystem.cs
+++ Assets/Scripts/DOTS/Systems/ParticleCollisionParentingSystem.cs
@@ -19,13 +19,14 @@ namespace com.TUDublin.VRContaminationSimulation.DOTS.Systems {
 
         protected override void OnUpdate() {
 
-            var ecb = _entityCommandBufferSystem.CreateCommandBuffer();
+            var ecb = _entityCommandBufferSystem.CreateCommandBuffer().AsParallelWriter();
 
             Entities
-                .WithoutBurst()
+                .WithName("VirusParticleInitialSticking")
+                .WithBurst()
                 .WithAll<VirusParticleData, StickyParticleData>()
                 .WithNone<Parent, LocalToParent>()
-                .ForEach((Entity entity, ref PhysicsVelocity pv, ref DecayingParticleData decayData, ref BrownianMotionData motionData, in DynamicBuffer<StatefulCollisionEvent> collisionBuffer) => {
+                .ForEach((Entity entity, int entityInQueryIndex, ref PhysicsVelocity pv, ref DecayingParticleData decayData, ref BrownianMotionData motionData, in DynamicBuffer<StatefulCollisionEvent> collisionBuffer) => {
                     if (collisionBuffer.IsEmpty) {
                         return;
                     }
@@ -44,19 +45,46 @@ namespace com.TUDublin.VRContaminationSimulation.DOTS.Systems {
                     
                     var other = collisionBuffer[collisionIndex].GetOtherCollisionEntity(entity);
                     
-                    ecb.AddComponent(entity, new Parent() {Value = other});
-                    ecb.AddComponent(entity, new LocalToParent());
+                    ecb.AddComponent(entityInQueryIndex, entity, new Parent() {Value = other});
+                    ecb.AddComponent(entityInQueryIndex,entity, new LocalToParent());
                     pv = new PhysicsVelocity();
-                    ecb.RemoveComponent<PhysicsVelocity>(entity);
-                    ecb.RemoveComponent<PhysicsMass>(entity);
-                    ecb.RemoveComponent<PhysicsDamping>(entity);
+                    ecb.RemoveComponent<PhysicsVelocity>(entityInQueryIndex, entity);
+                    ecb.RemoveComponent<PhysicsMass>(entityInQueryIndex, entity);
+                    ecb.RemoveComponent<PhysicsDamping>(entityInQueryIndex, entity);
                     
                     motionData.enabled = 0;
                     decayData.isDecayingParticle = 0;
+                }).ScheduleParallel();
 
-                }).Schedule();
-            
-            
+            Entities
+                .WithName("VirusParticleTransferSticking")
+                .WithBurst()
+                .WithAll<VirusParticleData, StickyParticleData, LocalToParent>()
+                .ForEach((Entity entity, int entityInQueryIndex, in DynamicBuffer<StatefulCollisionEvent> collisionBuffer, in Parent connectedBody) => {
+                    if (collisionBuffer.IsEmpty) {
+                        return;
+                    }
+
+                    int collisionIndex = -1;
+                    for (int i = 0; i < collisionBuffer.Length; i++) {
+                        if (collisionBuffer[i].CollisionState == CollisionEventState.Enter && collisionBuffer[i].GetOtherCollisionEntity(entity) != connectedBody.Value) {
+                            collisionIndex = i;
+                            break;
+                        }
+                    }
+
+                    if (collisionIndex < 0 ) {
+                        return;
+                    }
+                    
+                    var other = collisionBuffer[collisionIndex].GetOtherCollisionEntity(entity);
+                    
+                    ecb.RemoveComponent<Parent>(entityInQueryIndex, entity);
+                    ecb.RemoveComponent<LocalToParent>(entityInQueryIndex, entity);
+                    ecb.AddComponent(entityInQueryIndex, entity, new Parent() {Value = other});
+                    ecb.AddComponent(entityInQueryIndex,entity, new LocalToParent());
+                    
+                }).ScheduleParallel();
             
             _entityCommandBufferSystem.AddJobHandleForProducer(Dependency);
             
